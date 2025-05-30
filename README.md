611a27be937103fff90c41a4072a196431a7abe5
Gautam Kumar <gautam.kumar@baar.ai>
2025-05-31 01:11:42 +0530

    Add card loading skeleton, to the next one after 5 seconds, and progress bar

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ It is Instagram like story viewer built using **React.js** and **TypeScript**.
 ##  Features
 
 A list of stories visible in a smaller view in a horizontally scrollable list on home page.
+
+The user should be able to manually navigate between stories using UI controls. Tapping on the left side of an open story should take the user to the previous story. Tapping on the right side of an open story should take the user to the next story
+
+Automatically advance to the next one after 5 seconds
--- src/components/StoryFullView.tsx
+++ src/components/StoryFullView.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { MouseEvent } from "react"
 import './style.css';
+import CardSkeleton from './CardSkeleton';
 
 
 
@@ -16,12 +17,14 @@ interface StoryFullViewProps {
 const StoryFullView: React.FC<StoryFullViewProps> = ({ activeStories, currentIndex, handleClose, handleNext, handlePrev }) => {
 
     const [activeStoryIndex, setActiveStoryIndex] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false);
+    console.log(activeStories, activeStoryIndex)
 
     function handleImgClick(e: React.MouseEvent<HTMLDivElement>) {
         const width = e.currentTarget.offsetWidth
         const clintX = e.clientX
 
-        console.log(width, 'width', clintX, 'clintX')
+        // console.log(width, 'width', clintX, 'clintX')
 
         const isLeftClick = clintX > width / 2
 
@@ -33,19 +36,49 @@ const StoryFullView: React.FC<StoryFullViewProps> = ({ activeStories, currentInd
 
     }
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (activeStoryIndex < activeStories.length - 1) {
+                setActiveStoryIndex((prev) => prev + 1)
+            } else {
+                handleNext()
+                setActiveStoryIndex(0)
+            }
+        }, 5000)
+        return () => clearTimeout(timer)
+    }, [activeStories, activeStoryIndex])
+
+
+    useEffect(() => {
+        setLoading(true);
+        const loadTimeout = setTimeout(() => {
+            setLoading(false);
+        }, 500);
+        setActiveStoryIndex(0);
+
+        return () => clearTimeout(loadTimeout)
+    }, [currentIndex]);
+
+
     return (
         <div className='fullViewContainer' onClick={(e) => handleImgClick(e)}>
-            <div className='fullViewImgWrapper'>
+            {loading ? <CardSkeleton height='100%' /> : <div className='fullViewImgWrapper'>
                 <img
                     src={activeStories[activeStoryIndex]}
                     alt="story"
                     width="100%"
                     height="100%"
+                    loading="lazy" 
                 />
                 <button className="closeBtn" onClick={(e) => handleClose(e)}>
                     ✕
                 </button>
-            </div>
+                <div className='progressBarContainer'>
+                    <div key={activeStoryIndex} className='progressBar'>
+                    </div>
+                </div>
+            </div>}
+
         </div>
     )
 }
